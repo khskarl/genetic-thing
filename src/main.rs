aa7bd9fbd2b6668fa114d6899102313caeb078df
Eu <karllhenning64@gmail.com>
2017-03-11 18:09:16 -0300

    Added crossover function to Population struct

--- src/main.rs
+++ src/main.rs
@@ -4,11 +4,16 @@ use population::Range;
 
 // Everything else
 fn main() {
-    let population_size = 100;
-    let genome_size = 10;
-    let range = Range::new(0, 2);
-    let population = Population::<u8>::new(population_size, genome_size, range);
+    let population_size = 2;
+    let genome_size = 4;
+    let range = Range::new(0, 10);
+    let mut population = Population::<u8>::new(population_size, genome_size, range);
 
     println!("{:?}", population.individuals[0]);
-    println!("Length: {}", population.genome_size());
+    println!("{:?}", population.individuals[1]);
+    population.quick();
+    println!("{:?}", population.individuals[0]);
+    println!("{:?}", population.individuals[1]);
+
+    //population.crossover(population.individuals[0], population.individuals[1]); 
 }
--- src/population.rs
+++ src/population.rs
@@ -1,6 +1,7 @@
 extern crate rand;
 use self::rand::distributions::IndependentSample;
 pub use self::rand::distributions::Range;
+use std::cmp;
 
 // Individual Stuff
 #[derive(Debug)]
@@ -9,7 +10,7 @@ pub struct Individual<T> {
 }
 
 impl<T> Individual<T> {
-    pub fn new(size: u32, range: &Range<T>) -> Individual<T>
+    pub fn new(size: usize, range: &Range<T>) -> Individual<T>
         where T: rand::Rand + rand::distributions::range::SampleRange
     {
         let mut genome: Vec<T> = Vec::new();
@@ -28,10 +29,13 @@ impl<T> Individual<T> {
 pub struct Population<T> {
     pub individuals: Vec<Individual<T>>,
     range: Range<T>,
+    genome_length: usize,
 }
 
-impl<T> Population<T> {
-    pub fn new(size: u32, genome_size: u32, range: Range<T>) -> Population<T>
+impl<T> Population<T>
+    where T: Copy
+{
+    pub fn new(size: u32, genome_size: usize, range: Range<T>) -> Population<T>
         where T: rand::Rand + rand::distributions::range::SampleRange
     {
         let mut individuals: Vec<Individual<T>> = Vec::new();
@@ -43,10 +47,30 @@ impl<T> Population<T> {
         Population::<T> {
             individuals: individuals,
             range: range,
+            genome_length: genome_size,
         }
     }
 
-    pub fn genome_size(&self) -> usize {
-        self.individuals.len()
+
+    pub fn quick(&mut self) {
+        self.crossover(0, 1);
+    }
+
+    // TODO: Optimize this function to make temporary copy the shorter old slice
+    //currently it only the left slice regardless of length.
+    fn crossover(&mut self, index_dad: usize, index_mom: usize) {
+        let mut rng = rand::thread_rng();
+        let range = Range::new(1, self.genome_length - 1);
+        let point = range.ind_sample(&mut rng);
+
+        let max_index = cmp::max(index_dad, index_mom);
+        let min_index = cmp::min(index_dad, index_mom);
+
+        let (split_left, split_right) = self.individuals.split_at_mut(max_index);
+        let (dad, mom) = (&mut split_left[min_index], &mut split_right[0]);
+
+        let old_left_slice_dad = Vec::from(&dad.genome[0..point + 1]);
+        dad.genome[0..point + 1].copy_from_slice(&mom.genome[0..point + 1]);
+        mom.genome[0..point + 1].copy_from_slice(&old_left_slice_dad);
     }
 }
