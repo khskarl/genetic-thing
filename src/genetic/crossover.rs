4933b7a747c7cce54eb8e83a0723c888eb84f3eb
khskarl <karllhenning64@gmail.com>
2017-04-12 21:41:42 -0300

    Implemented PMX

--- src/genetic/crossover.rs
+++ src/genetic/crossover.rs
@@ -22,3 +22,44 @@ pub fn one_point_crossover<T>(dad_genome: &Vec<T>, mom_genome: &Vec<T>) -> (Vec<
 
     (boy_genome, girl_genome)
 }
+
+// Permutation crossovers
+// TODO: Test the dank out of this function
+pub fn partially_matched_crossover<T>(dad_genome: &Vec<T>, mom_genome: &Vec<T>) -> (Vec<T>, Vec<T>)
+    where T: Copy + PartialEq
+{
+    let mut rng = rand::thread_rng();
+    let start_index = Range::new(1, dad_genome.len() / 2).ind_sample(&mut rng);
+    let end_index = Range::new(dad_genome.len() / 2, dad_genome.len() - 1).ind_sample(&mut rng);
+    
+    let mut boy_genome = dad_genome.clone();
+    let mut girl_genome = mom_genome.clone();
+    boy_genome[start_index..end_index + 1].copy_from_slice(&mom_genome[start_index..end_index + 1]);
+    girl_genome[start_index..end_index + 1].copy_from_slice(&dad_genome[start_index..end_index + 1]);
+
+    for i in 0..start_index {
+        for j in start_index..end_index {
+            if boy_genome[i] == boy_genome[j] {
+                boy_genome[i] = girl_genome[j];
+            }
+            
+            if girl_genome[i] == girl_genome[j] {
+                girl_genome[i] = boy_genome[j];
+            }
+        }
+    }
+    
+    for i in end_index..mom_genome.len() {
+        for j in start_index..end_index {
+            if boy_genome[i] == boy_genome[j] {
+                boy_genome[i] = girl_genome[j];
+            }
+            
+            if girl_genome[i] == girl_genome[j] {
+                girl_genome[i] = boy_genome[j];
+            }
+        }
+    }
+    
+    (boy_genome, girl_genome)
+}
